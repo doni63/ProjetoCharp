366d4af018e0ad0571ca443b00474ec41b9ad108
doni63 <dbatistajunior63@gmail.com>
2024-01-28 13:09:53 -0300

    Criando SellerControler, view da Seller

---
+++ SallerWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SallerWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
