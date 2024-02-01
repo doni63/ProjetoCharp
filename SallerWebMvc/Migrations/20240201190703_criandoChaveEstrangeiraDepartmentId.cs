65a15ee1ca01fed885730dde5a49dbbc476b577f
doni63 <dbatistajunior63@gmail.com>
2024-02-01 17:02:00 -0300

    Foreign key not null (referential integrity)

---
+++ SallerWebMvc/Migrations/20240201190703_criandoChaveEstrangeiraDepartmentId.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SallerWebMvc.Migrations
+{
+    public partial class criandoChaveEstrangeiraDepartmentId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- SallerWebMvc/Models/Seller.cs
+++ SallerWebMvc/Models/Seller.cs
@@ -8,6 +8,7 @@ public class Seller
     public DateTime BirthDate { get; set; }
     public double Salary { get; set; }
     public Department? Department { get; set; } //associação um vendedor esta em um departamento
+    public int DepartmentId { get; set; }
     public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>(); //um vendedor pode ter varias vendas
     public Seller() { }
 
